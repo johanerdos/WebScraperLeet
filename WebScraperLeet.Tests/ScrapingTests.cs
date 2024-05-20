50374d32ffd104026bac0f0bdc767d7d32517d7a
Johan Erdös <johan.erdos@forefront.se>
2024-05-20 17:35:11 +0200

    add placeholder unit tests

--- WebScraperLeet.Tests/ScrapingTests.cs
+++ WebScraperLeet.Tests/ScrapingTests.cs
@@ -10,6 +10,36 @@ namespace WebScraperLeet.Tests
     [TestFixture]
     public class ScrapingTests
     {
+        private readonly List<string> _rootUrlsToScrape = new List<string>()
+        {
+            "/page-1.html",
+            "/page-2.html",
+            "/page-3.html"
+        };
 
+
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public async Task Should_Fetch_All_Pages()
+        {
+            Assert.That(_rootUrlsToScrape, Does.Contain("someRootUrl"));
+        }
+
+        [Test]
+        public async Task Should_Fetch_All_Inner_Pages()
+        {
+            var expectedInnerPagesToScrape = new List<string>()
+            {
+                "/some_book.index.html",
+                "/my_book.index.html"
+            };
+
+            Assert.That(_rootUrlsToScrape.Contains("someRootUrl") || expectedInnerPagesToScrape.Contains("someInnerUrl"), Is.True);
+        }
     }
 }
